1eff5e508f83d921d84a42bf9e0274438f135ba7
Katia Priazhnikova <milightning.ua@gmail.com>
2024-01-26 21:35:07 +0200

    program for change calcultion - 'do while' loop checks if the change value is more than 0 - 'while' loops count the amount of quarters, dimes, nickels and pennies

--- cash/cash.c
+++ cash/cash.c
@@ -16,26 +16,30 @@ int main(void)
     int nickels = 0;
     int pennies = 0;
 
-    while (change >= 25)
+        while (change >= 25)
     {
         quarters++;
         change -= 25;
     }
-    while (change >= 10)
+
+        while (change >= 10)
     {
         dimes++;
         change -= 10;
     }
-    while (change >= 5)
+        
+        while (change >= 5)
     {
         nickels++;
         change -= 5;
     }
-    while (change >= 1)
+    
+        while (change >= 1)
     {
         pennies++;
         change -= 1;
     }
+    
     int amountOfCoins = quarters + dimes + nickels + pennies;
     printf("%i\n", amountOfCoins);
 }
