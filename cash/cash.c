bb9cf9057c424975c9d2860047c994e7bb58b0dd
Kate Priazhnikova <milightning.ua@gmail.com>
2024-01-31 13:30:08 +0200

    Add a program for calculating change (#4)
    * Add a program for calculating change
    
    Add .gitignore file
    
    'do while' loop checks if the value of the change is more than 0
    
    'while' loops counts the amount of quarters, dimes, nickels and pennies
    
    * Remove .DS_Store file
    
    Update style of cash.c accordingly to style50 checkup

---
+++ cash/cash.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int change;
+
+    do
+    {
+        change = get_int("Change owed: ");
+    }
+    while (change <= 0);
+
+    int quarters = 0;
+    int dimes = 0;
+    int nickels = 0;
+    int pennies = 0;
+
+    while (change >= 25)
+    {
+        quarters++;
+        change -= 25;
+    }
+
+    while (change >= 10)
+    {
+        dimes++;
+        change -= 10;
+    }
+
+    while (change >= 5)
+    {
+        nickels++;
+        change -= 5;
+    }
+
+    while (change >= 1)
+    {
+        pennies++;
+        change -= 1;
+    }
+    int amountOfCoins = quarters + dimes + nickels + pennies;
+    printf("%i\n", amountOfCoins);
+}
